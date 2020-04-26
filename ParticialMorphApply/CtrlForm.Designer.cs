7b0cd6cd86bcaa3f205141af0beb51bb5bf9803a
Inwerwm <saclainvervm@gmail.com>
2020-04-26 20:31:25 +0900

    プロジェクトの生成

---
+++ ParticialMorphApply/CtrlForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ParticialMorphApply
+{
+    partial class CtrlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeViewMorph = new System.Windows.Forms.TreeView();
+            this.trackBarMorphRatio = new System.Windows.Forms.TrackBar();
+            this.textBoxMorphRatio = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMorphRatio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeViewMorph
+            // 
+            this.treeViewMorph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeViewMorph.Location = new System.Drawing.Point(0, 0);
+            this.treeViewMorph.Margin = new System.Windows.Forms.Padding(5);
+            this.treeViewMorph.Name = "treeViewMorph";
+            this.treeViewMorph.Size = new System.Drawing.Size(300, 661);
+            this.treeViewMorph.TabIndex = 0;
+            this.treeViewMorph.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewMorph_AfterSelect);
+            // 
+            // trackBarMorphRatio
+            // 
+            this.trackBarMorphRatio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarMorphRatio.LargeChange = 50;
+            this.trackBarMorphRatio.Location = new System.Drawing.Point(300, 33);
+            this.trackBarMorphRatio.Maximum = 1000;
+            this.trackBarMorphRatio.Name = "trackBarMorphRatio";
+            this.trackBarMorphRatio.Size = new System.Drawing.Size(200, 45);
+            this.trackBarMorphRatio.SmallChange = 10;
+            this.trackBarMorphRatio.TabIndex = 1;
+            this.trackBarMorphRatio.TickFrequency = 100;
+            this.trackBarMorphRatio.Scroll += new System.EventHandler(this.trackBarMorphRatio_Scroll);
+            // 
+            // textBoxMorphRatio
+            // 
+            this.textBoxMorphRatio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMorphRatio.Location = new System.Drawing.Point(300, 0);
+            this.textBoxMorphRatio.Name = "textBoxMorphRatio";
+            this.textBoxMorphRatio.Size = new System.Drawing.Size(200, 27);
+            this.textBoxMorphRatio.TabIndex = 2;
+            this.textBoxMorphRatio.Text = "0";
+            this.textBoxMorphRatio.TextChanged += new System.EventHandler(this.textBoxMorphRatio_TextChanged);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRun.Location = new System.Drawing.Point(300, 611);
+            this.buttonRun.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(200, 50);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "選択頂点に適用";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            // 
+            // CtrlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 661);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.textBoxMorphRatio);
+            this.Controls.Add(this.trackBarMorphRatio);
+            this.Controls.Add(this.treeViewMorph);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(516, 155);
+            this.Name = "CtrlForm";
+            this.Text = "CtrlForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CtrlForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMorphRatio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewMorph;
+        private System.Windows.Forms.TrackBar trackBarMorphRatio;
+        private System.Windows.Forms.TextBox textBoxMorphRatio;
+        private System.Windows.Forms.Button buttonRun;
+    }
+}
---
+++ ParticialMorphApply/CtrlForm.cs
@@ -0,0 +1,130 @@
+﻿using PEPlugin;
+using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticialMorphApply
+{
+    public partial class CtrlForm : Form
+    {
+        IPERunArgs args;
+        IPXPmx pmx;
+
+        TreeNode nodeBrow;
+        TreeNode nodeEye;
+        TreeNode nodeLip;
+        TreeNode nodeOther;
+        private float ratio;
+        private int selectedMorphID;
+
+        private int SelectedMorphID
+        {
+            get => selectedMorphID;
+            set
+            {
+                selectedMorphID = value;
+                args.Host.Connector.View.TransformView.SelectedMorphIndex = selectedMorphID;
+            }
+        }
+        private float Ratio
+        {
+            get => ratio;
+            set
+            {
+                ratio = value;
+                if (SelectedMorphID != -1)
+                {
+                    args.Host.Connector.View.TransformView.SelectedMorphIndex = SelectedMorphID;
+                    args.Host.Connector.View.TransformView.MorphValue = ratio;
+                }
+            }
+        }
+
+        public CtrlForm(IPERunArgs input)
+        {
+            InitializeComponent();
+            args = input;
+            Format();
+
+            nodeBrow = new TreeNode("眉");
+            nodeEye = new TreeNode("目");
+            nodeLip = new TreeNode("口");
+            nodeOther = new TreeNode("他");
+            treeViewMorph.Nodes.AddRange(new TreeNode[] { nodeBrow, nodeEye, nodeLip, nodeOther });
+
+            SelectedMorphID = -1;
+        }
+
+        public void Format()
+        {
+            pmx = args.Host.Connector.Pmx.GetCurrentState();
+            makeMorphTree();
+        }
+
+        private void makeMorphTree()
+        {
+            nodeEye.Nodes.Clear();
+            nodeLip.Nodes.Clear();
+            nodeBrow.Nodes.Clear();
+            nodeOther.Nodes.Clear();
+
+            // 頂点モーフを分類して木構造に追加
+            foreach (var m in pmx.Morph.Where(m => m.Kind == MorphKind.Vertex))
+            {
+                var n = new TreeNode(m.Name);
+                n.Tag = m;
+
+                switch (m.Panel)
+                {
+                    case 1:
+                        nodeBrow.Nodes.Add(n);
+                        break;
+                    case 2:
+                        nodeEye.Nodes.Add(n);
+                        break;
+                    case 3:
+                        nodeLip.Nodes.Add(n);
+                        break;
+                    case 4:
+                        nodeOther.Nodes.Add(n);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void CtrlForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Visible = false;
+            }
+        }
+
+        private void textBoxMorphRatio_TextChanged(object sender, EventArgs e)
+        {
+            Ratio = float.Parse(textBoxMorphRatio.Text);
+            trackBarMorphRatio.Value = (int)(Ratio * 1000);
+        }
+
+        private void trackBarMorphRatio_Scroll(object sender, EventArgs e)
+        {
+            Ratio = trackBarMorphRatio.Value / 1000.0f;
+            textBoxMorphRatio.Text = Ratio.ToString("F3");
+        }
+
+        private void treeViewMorph_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SelectedMorphID = treeViewMorph.SelectedNode.Level < 2 ? -1 : pmx.Morph.IndexOf((IPXMorph)treeViewMorph.SelectedNode.Tag);
+        }
+    }
+}
